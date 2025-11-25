99aef373e00a5c4d7bd812a0119afe042f6c427e
manish mainali <manishmainali343@gmail.com>
2025-11-25 09:23:38 +0545

    Update 23_smallest_number.js

--- 23_smallest_number.js
+++ 23_smallest_number.js
@@ -1,6 +1,6 @@
 // smallest number . lets keep an array and find smallest number in it
 let array = [3, 5, 7, 2, 8, -1];
-let min = array[0]; // assume first element is the smallest
+let min = array[0]; // assume first element is the smallest number
  array.forEach(function(num){
     if(num < min){
         min = num;
